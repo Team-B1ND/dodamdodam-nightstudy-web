daee3572b9790b9e8925d4a45520302c49f64b3d
ftery0 <lerb5253@dgsw.hs.kr>
2024-12-05 20:31:08 +0900

    refactor :: 학습장소 삭제 #24

--- src/components/Apply/ApplyPlace/index.tsx
+++ src/components/Apply/ApplyPlace/index.tsx
@@ -1,9 +1,8 @@
-import { ChangeEvent, Suspense } from "react";
+import { ChangeEvent } from "react";
 import ApplyRequireText from "../../Common/RequireText";
 import ApplyLargeText from "../../Common/Apply/ApplyText";
 import ApplyTitle from "../../Common/Apply/ApplyTitle";
 import {
-  ApplyPlaceBox,
   ApplyPlaceContainer,
   ApplyPlaceFlex,
   ApplyStudyContentBox,
@@ -11,9 +10,7 @@ import {
 } from "./style";
 import { Apply } from "../../../types/Apply/apply.type";
 import Box from "../../Common/Apply/ApplyBox";
-import ApplyPlaceItem from "./ApplyPlaceItem";
-import CheckBoxFallBackLoader from "../../Common/FallbackLoader/CheckBox";
-import { ErrorBoundary } from "react-error-boundary";
+
 
 interface Props {
   checkOnlyOne: (e: ChangeEvent<HTMLInputElement>) => void;
@@ -27,23 +24,8 @@ const ApplyPlace = ({ checkOnlyOne, postData, onChangeContent }: Props) => {
       <ApplyTitle>학습정보</ApplyTitle>
       <Box
         size="large"
-        style={{ display: "flex", flexDirection: "column", rowGap: "23px" }}
+        style={{ display: "flex", flexDirection: "column", rowGap: "10px" }}
       >
-        <ApplyPlaceBox>
-          <ApplyPlaceFlex>
-            <ApplyRequireText htmlFor="place">* </ApplyRequireText>
-            <ApplyLargeText htmlFor="place">학습장소</ApplyLargeText>
-            <ApplyRequireText htmlFor="place">
-              *하나만 선택해주세요.
-            </ApplyRequireText>
-          </ApplyPlaceFlex>
-          <ErrorBoundary fallback={<>error</>}>
-            <Suspense fallback={<CheckBoxFallBackLoader />}>
-              <ApplyPlaceItem checkOnlyOne={checkOnlyOne} />
-            </Suspense>
-          </ErrorBoundary>
-        </ApplyPlaceBox>
-
         <ApplyStudyContentBox>
           <ApplyPlaceFlex>
             <ApplyRequireText htmlFor="content">* </ApplyRequireText>
