14b4419a6fe284a6b7b56798dee2b84dfa760f2b
developersihyeon <sihyeonsky@gmail.com>
2025-03-31 16:53:53 +0900

    feat :: April Fool's Day

--- src/components/Common/ThemeProviderContainer/index.tsx
+++ src/components/Common/ThemeProviderContainer/index.tsx
@@ -1,6 +1,11 @@
 import { ReactNode } from "react";
 import useTheme from "hooks/Theme/useTheme";
 import { DodamThemeProvider, DodamGlobalStyles } from "@b1nd/dds-web";
+import { ThemeProvider } from "styled-components";
+import {
+  April_Fools_Day_Dark_Theme,
+  April_Fools_Day_Light_Theme,
+} from "style/theme";
 
 interface Props {
   children: ReactNode;
@@ -10,10 +15,17 @@ const ThemeProviderContainer = ({ children }: Props) => {
   const { themeColor } = useTheme();
 
   return (
-    <DodamThemeProvider theme={themeColor}>
+    // <DodamThemeProvider theme={themeColor}>
+    <ThemeProvider
+      theme={
+        themeColor === "LIGHT"
+          ? April_Fools_Day_Light_Theme
+          : April_Fools_Day_Dark_Theme
+      }>
       <DodamGlobalStyles />
       {children}
-    </DodamThemeProvider>
+    </ThemeProvider>
+    // </DodamThemeProvider>
   );
 };
 export default ThemeProviderContainer;
