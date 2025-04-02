f7268d546e647c1e2789dafcfabd4a5d68ad5761
developersihyeon <sihyeonsky@gmail.com>
2025-04-02 09:56:29 +0900

    feat :: 도담 정상화

--- src/components/Common/ThemeProviderContainer/index.tsx
+++ src/components/Common/ThemeProviderContainer/index.tsx
@@ -1,11 +1,6 @@
 import { ReactNode } from "react";
 import useTheme from "hooks/Theme/useTheme";
 import { DodamThemeProvider, DodamGlobalStyles } from "@b1nd/dds-web";
-import { ThemeProvider } from "styled-components";
-import {
-  April_Fools_Day_Dark_Theme,
-  April_Fools_Day_Light_Theme,
-} from "style/theme";
 
 interface Props {
   children: ReactNode;
@@ -15,17 +10,10 @@ const ThemeProviderContainer = ({ children }: Props) => {
   const { themeColor } = useTheme();
 
   return (
-    // <DodamThemeProvider theme={themeColor}>
-    <ThemeProvider
-      theme={
-        themeColor === "LIGHT"
-          ? April_Fools_Day_Light_Theme
-          : April_Fools_Day_Dark_Theme
-      }>
+    <DodamThemeProvider theme={themeColor}>
       <DodamGlobalStyles />
       {children}
-    </ThemeProvider>
-    // </DodamThemeProvider>
+    </DodamThemeProvider>
   );
 };
 export default ThemeProviderContainer;
