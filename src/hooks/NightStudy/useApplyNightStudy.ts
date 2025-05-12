5d56c13b56d7aebc832a95c1cd4a4973ad7a695b
Cartridge0923 <pjmin0923@gmail.com>
2025-05-12 21:49:01 +0900

    fix : split to constant #39

--- src/hooks/NightStudy/useApplyNightStudy.ts
+++ src/hooks/NightStudy/useApplyNightStudy.ts
@@ -63,12 +63,7 @@ export const useApplyNightStudy = (isPersonalPage : boolean) => {
       description: "",
       students: [],
     });
-    setPlaceData([
-      { id: 1, name: "LAB_12", title: "랩 12실", isAtv: false },
-      { id: 2, name: "LAB_13", title: "랩 13실", isAtv: false },
-      { id: 3, name: "LAB_14", title: "랩 14실", isAtv: false },
-      { id: 4, name: "LAB_15", title: "랩 15실", isAtv: false },
-    ])
+    setPlaceData(PLACE_ITEMS)
   }
 
   // 프로젝트 인원 선택 함수
