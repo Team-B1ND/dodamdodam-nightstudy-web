011f3c07b6667821deb55ee130b23759110e7bd3
Cartridge0923 <pjmin0923@gmail.com>
2025-05-19 17:42:02 +0900

    fix : make logic more simple #48

--- src/utils/Error/errorHandler.ts
+++ src/utils/Error/errorHandler.ts
@@ -21,33 +21,16 @@ class ErrorHandler {
     }
   };
 
-  public deleteMyNightStudy = (error: AxiosError) => {
+  public deleteMyNightStudy = (error: AxiosError, type: "PERSONAL" | "PROJECT") => {
     const status = error.response?.status;
 
     switch (status) {
       case 400:
         return B1ndToast.showError("유효한 Enum 값이 아닙니다.");
       case 403:
-        return B1ndToast.showError("심자 신청자가 아닙니다.")
+        return B1ndToast.showError(type === "PERSONAL" ? "심자 신청자가 아닙니다." : "프로젝트 리더가 아닙니다.")
       case 404:
         return B1ndToast.showError("해당 심자가 없습니다.")
-      case 500:
-        return B1ndToast.showError("서버 에러");
-      default:
-        return B1ndToast.showError("내 심자 삭제에 실패하였습니다.");
-    }
-  };
-
-  public deleteMyProjcetNightStudy = (error: AxiosError) => {
-    const status = error.response?.status;
-
-    switch (status) {
-      case 400:
-        return B1ndToast.showError("유효한 Enum 값이 아닙니다.");
-      case 403:
-        return B1ndToast.showError("프로젝트 리더가 아닙니다.")
-      case 404:
-        return B1ndToast.showError("해당 심자가 없습니다")
       case 500:
         return B1ndToast.showError("서버 에러");
       default:
