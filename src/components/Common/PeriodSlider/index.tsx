5ff1bb18b529b0f010240169beb4535af8aedfda
전민찬 <132974029+wjzlskxk@users.noreply.github.com>
2025-09-02 11:17:12 +0900

    !HOTFIX:: nightstudy reorganization

--- src/components/Common/PeriodSlider/index.tsx
+++ src/components/Common/PeriodSlider/index.tsx
@@ -1,17 +1,19 @@
-
-import * as S from "./style";
-import DynamicBounds from 'rc-slider';
+import * as S from './style'
+import DynamicBounds from 'rc-slider'
 import './rc-slider.css'
 
 interface PeriodSliderType {
   handlePersonalType: (type: number) => void
 }
 
-const PeriodSlider = ({handlePersonalType}: PeriodSliderType) => {
+const PeriodSlider = ({ handlePersonalType }: PeriodSliderType) => {
   return (
     <S.PeriodSliderContainer>
       심자 시간 선택
-      <p>주의 : 심자 2 신청 시 심자 1 또한 필수 참석해야 합니다. 또한 9시 50분부터는 정비시간입니다.</p>
+      <p>
+        주의 : 심자 2 신청 시 심자 1 또한 필수 참석해야 합니다. 또한 9시
+        50분부터는 정비시간입니다.
+      </p>
       <S.PeriodSliderContent>
         <S.PeriodIndicator>
           <S.PeriodText>
@@ -22,12 +24,13 @@ const PeriodSlider = ({handlePersonalType}: PeriodSliderType) => {
             <p>심자2</p>
             <span>11:50까지</span>
           </S.PeriodText>
-          <S.PeriodText>
-            <p>심자3</p>
-            <span>12:30까지</span>
-          </S.PeriodText>
         </S.PeriodIndicator>
-        <DynamicBounds dots min={1} max={3} onChange={(value) => handlePersonalType(value as number)}/>
+        <DynamicBounds
+          dots
+          min={1}
+          max={2}
+          onChange={(value) => handlePersonalType(value as number)}
+        />
       </S.PeriodSliderContent>
     </S.PeriodSliderContainer>
   )
